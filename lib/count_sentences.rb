18fa5c384faa97ce39bc1ec956d268453444a220
Amanda King <amapickell@gmail.com>
2019-06-27 02:04:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,6 @@ class String
 
   def count_sentences
     self.split(/(.!?)+/)
-    binding.pry
   end
 end
 
-"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
